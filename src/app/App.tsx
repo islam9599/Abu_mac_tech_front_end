f8e8ec231cbc272a46c69641490b088040973701
Islombek <eislombek9599@gmail.com>
2024-01-12 19:30:05 +0900

    Feat: setup MUI integration for the  project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "../css/App.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 
 function App() {
-  return <div className="App">Assalomu alaykum</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h1"} gutterBottom>
+            Create React App on Typscript with Redux
+          </Typography>
+        </Box>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
