798f432800a716b82ae7be1042942fbac5c21e3c
Islombek <eislombek9599@gmail.com>
2024-02-21 17:18:15 +0900

    Feat: publish community screen page component

--- src/app/screens/CommunityPage/index.tsx
+++ src/app/screens/CommunityPage/index.tsx
@@ -1,10 +1,146 @@
-import { Container } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import "../../../css/community.css";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { TargetArticles } from "./targetArticles";
+import Marginer from "../../component/marginer";
+import { Home } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
-export const CommunityPage = () => {
+const targetBoArticles = [1, 2, 3, 4, 5];
+
+export function CommunityPage() {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("1");
+  const handleChange = (event: any, newValue: string) => {
+    setValue(newValue);
+  };
+  const navigateToHomeHandler = () => {
+    navigate("/");
+  };
   return (
-    <Container>
-      <h1>CommunityPage</h1>
-    </Container>
+    <div className="community_page">
+      <div
+        style={{
+          display: "flex",
+          // flexDirection: "row",
+          // alignItems: "center",
+          // justifyContent: "space-between",
+          width: "100%",
+          height: "auto",
+        }}
+      >
+        <Container>
+          <Stack className="community_page_wrapper">
+            <Stack flexDirection={"row"} alignItems={"center"} mr={75}>
+              <Home sx={{ width: "29px", height: "29px" }} />
+              <Typography sx={{ margin: "15px" }} variant="h4">
+                Home
+              </Typography>
+              <Marginer width="1" height="20" bg="#000" direction="vertical" />
+
+              <Typography sx={{ margin: "15px" }} variant="h4">
+                Community
+                <a
+                  href=""
+                  onClick={navigateToHomeHandler}
+                  style={{
+                    textDecoration: "none",
+                    cursor: "pointer",
+                    marginLeft: "5px",
+                    color: "none",
+                  }}
+                >
+                  x
+                </a>
+              </Typography>
+            </Stack>
+            <Stack className="tab_main_wrapper">
+              <TabContext value={value}>
+                <Box className="order_nav_frame">
+                  <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <TabList
+                      onChange={handleChange}
+                      aria-label="basic tabs example"
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        maxWidth: "100%",
+                      }}
+                      variant="fullWidth"
+                    >
+                      <Tab
+                        sx={{ fontWeight: "bold", fontSize: "13px" }}
+                        label="BARCHA MAQOLALAR"
+                        value="1"
+                      />
+                      <Tab
+                        sx={{ fontWeight: "bold", fontSize: "13px" }}
+                        label="MASHXURLAR"
+                        value="2"
+                      />
+                      <Tab
+                        sx={{ fontWeight: "bold", fontSize: "13px" }}
+                        label="OSHXONAGA BAHO"
+                        value="3"
+                      />
+                    </TabList>
+                  </Box>
+                </Box>
+                <Stack
+                  flexDirection={"row"}
+                  className="tabpanel_wrapper"
+                  // style={{ width: "100%", height: "980px", marginTop: "15px" }}
+                >
+                  {/* <Marginer
+                    width="863"
+                    height="1"
+                    bg="rgba(228, 228, 228, 0.83)"
+                  /> */}
+                  <Stack className="main_article">
+                    <TabPanel value="1">
+                      <TargetArticles targetBoArticles={[1, 2, 3, 4, 5, 6]} />
+                    </TabPanel>
+                    <TabPanel value="2">
+                      <TargetArticles targetBoArticles={[1, 2, 3, 4, 5]} />
+                    </TabPanel>
+                    <TabPanel value="3">
+                      <TargetArticles targetBoArticles={[1, 2]} />
+                    </TabPanel>
+                    <TabPanel value="4">
+                      <TargetArticles targetBoArticles={[1, 2]} />
+                    </TabPanel>
+                  </Stack>
+                </Stack>
+                {/* <Marginer width="863" height="1" bg="#007655" /> */}
+                <Stack className="aricle_pagination" spacing={4}>
+                  <Pagination
+                    count={3}
+                    page={1}
+                    renderItem={(item) => (
+                      <PaginationItem
+                        components={{
+                          previous: ArrowBackIcon,
+                          next: ArrowForwardIcon,
+                        }}
+                        {...item}
+                        color="secondary"
+                      />
+                    )}
+                  />
+                </Stack>
+              </TabContext>
+            </Stack>
+          </Stack>
+        </Container>
+      </div>
+    </div>
   );
-};
+}
---
+++ src/app/screens/CommunityPage/targetArticles.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Box, Link, Stack } from "@mui/material";
+import { Favorite, Person, RemoveRedEye } from "@mui/icons-material";
+
+export function TargetArticles(props: any) {
+  return (
+    <Stack>
+      <Stack className="bo_articles_container" flexDirection={"row"}>
+        {props.targetBoArticles.map((artciles: any, index: string) => {
+          return (
+            <Link className="target_articles_container">
+              <Stack
+                flexDirection={"column"}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Box>
+                  <img
+                    className="user_img"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkt4Kj0wBAcuPzs8HxRYPR3xFqd03u3JuEXw&usqp=CAU"
+                    alt=""
+                  />
+                </Box>
+                <Stack
+                  flexDirection={"column"}
+                  width={"90%"}
+                  height={"auto"}
+                  marginLeft={"15px"}
+                  marginTop={"15px"}
+                >
+                  <Box className="author_info">
+                    <Person sx={{ color: "#fff" }} />
+                    <span>@nurse</span>
+                  </Box>
+                  <Box className="article_desc">
+                    <span>Kebuli Rice with tomatoes s...</span>
+                  </Box>
+                  <Stack className="article_date">
+                    <p>23-11-23 19:46</p>
+                    <Favorite />
+                    <p>1</p>
+                    <RemoveRedEye />
+                    <p>2</p>
+                  </Stack>
+                </Stack>
+              </Stack>
+            </Link>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
+}
