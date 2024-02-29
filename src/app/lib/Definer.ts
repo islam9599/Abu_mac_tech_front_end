347f5753f2735d06d47d56581e4d04eef4b328f8
Islombek <eislombek9599@gmail.com>
2024-02-29 22:09:23 +0900

    Feat: develop sweetalert & .env logic

---
+++ src/app/lib/Definer.ts
@@ -0,0 +1,9 @@
+export const Definer = {
+  auth_err1: "att: please login first!",
+
+  input_err1: "att: please fulfill all inputs!",
+  input_err2: "att: please insert JPG, JPEG, PNG & WEBP formats only!",
+  input_err3: "att: message is empty!",
+
+  general_err1: "att: something went wrong!",
+};
