0052908569729f8fec6f36021b51c884d8822a81
Islombek <eislombek9599@gmail.com>
2024-03-22 23:50:43 +0900

    Fix: modify shippingInfo on Homepage screen

--- src/app/screens/Homepage/features.tsx
+++ src/app/screens/Homepage/features.tsx
@@ -1,16 +1,14 @@
 import { Container, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import Marginer from "../../component/marginer";
 
 export const Features = () => {
+  /** Initialization */
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   return (
     <div>
       <Container className="shipping_info_wrapper">
-        <Stack
-          flexDirection={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
+        <Stack className="shipping_info_category">
           <img
             className="shipping_info_img"
             src="/home/ic-delivery.svg"
@@ -19,12 +17,13 @@ export const Features = () => {
           <p className="shippin_info_title">Fast Worldwide Shipping</p>
           <p className="shippin_info_desc">Get free shipping over $250</p>
         </Stack>
-        <Marginer direction="horizontal" width="1" height="126" bg="gray" />
-        <Stack
-          flexDirection={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
+        {isMobile ? (
+          <Marginer direction="vertical" width="300" height="1" bg="gray" />
+        ) : (
+          <Marginer direction="horizontal" width="1" height="126" bg="gray" />
+        )}
+
+        <Stack className="shipping_info_category">
           <img
             src="/home/ic-call-center.svg"
             className="shipping_info_img"
@@ -33,24 +32,24 @@ export const Features = () => {
           <p className="shippin_info_title">24/7 Customer Support</p>
           <p className="shippin_info_desc">Friendly 24/7 customer support</p>
         </Stack>
-        <Marginer direction="horizontal" width="1" height="126" bg="gray" />
+        {isMobile ? (
+          <Marginer direction="vertical" width="300" height="1" bg="gray" />
+        ) : (
+          <Marginer direction="horizontal" width="1" height="126" bg="gray" />
+        )}
 
-        <Stack
-          flexDirection={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
+        <Stack className="shipping_info_category">
           <img src="/home/ic-shield.svg" className="shipping_info_img" alt="" />
           <p className="shippin_info_title">Money Back Guarantee</p>
           <p className="shippin_info_desc">We return money within 30 days</p>
         </Stack>
-        <Marginer direction="horizontal" width="1" height="126" bg="gray" />
+        {isMobile ? (
+          <Marginer direction="vertical" width="300" height="1" bg="gray" />
+        ) : (
+          <Marginer direction="horizontal" width="1" height="126" bg="gray" />
+        )}
 
-        <Stack
-          flexDirection={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
+        <Stack className="shipping_info_category">
           <img
             src="/home/ic-credit-card.svg"
             className="shipping_info_img"
