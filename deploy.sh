9a361535e6898e7240bd5a597180d9a66b0d4659
Islombek <eislombek9599@gmail.com>
2024-03-24 03:18:30 +0900

    Fix: modify deploy.sh & adding serve package init

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@
 #PRODUCTION
 git checkout master
 git reset --hard
-
 git pull origin master 
-
 npm i yarn -global
+yarn global add serve
 yarn
 yarn run build
 pm2 start "yarn run start:prod" --name=Abu_Mac_Tech-REACT
